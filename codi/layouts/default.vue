a137331f347901ac6a0053cda137dcac6128f1b3
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-03-18 23:42:09 +0100

    fin de course

---
+++ codi/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup>
+const user = useUser();
+</script>
+<template>
+  <nav style="display: flex; gap: 10px">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/movies">Movies</NuxtLink>
+    <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
+    <a href="#" v-else @click.prevent="user.isLoggedIn = false">Logout</a>
+  </nav>
+  <slot></slot>
+</template>
